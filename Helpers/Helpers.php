7afa205288bcb72b0fa1bde0f799526d0e39a6ca
Jean carlos <cusipumajean20@gmail.com>
2021-02-16 11:11:40 -0500

    v 10.0

--- Helpers/Helpers.php
+++ Helpers/Helpers.php
@@ -3,7 +3,9 @@
 function base_url(){
     return BASE_URL;
 }
-
+function media(){
+    return BASE_URL."Assets/";
+}
 //muestra informacion formateada
 function dep($data){
     $format = print_r('<pre>');
